c56d9a353461eb01177190a334d34f817c1527a5
Sourabh Chauhan <97791303+5chauhan@users.noreply.github.com>
2022-04-17 13:09:51 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,14 @@
+
 import React, {Component, useState} from "react";
 import '../styles/App.css';
 
 const App = () => {
   return (
-    <div id="main"></div>
+    <div id="main">
+      <h1 data-ns-test="project-name">Clone of IndiaMART</h1>
+      <p data-ns-test="project-description">I want to clone the IndiaMART app</p>
+    </div>
   )
 }
-
 
 export default App;
