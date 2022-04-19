83e51a5ef24332edf688fc28ccf86e1abce46240
Sourabh Chauhan <97791303+5chauhan@users.noreply.github.com>
2022-04-19 16:56:29 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,23 @@
-
-import React, {Component, useState} from "react";
-import '../styles/App.css';
+import React, { Component, useState } from "react";
+import "../styles/App.css";
 
 const App = () => {
   return (
-    <div id="main">
-      <h1 data-ns-test="project-name">Clone of IndiaMART</h1>
-      <p data-ns-test="project-description">I want to clone the IndiaMART app</p>
-    </div>
-  )
-}
+    <>
+      <h1 data-ns-test="project-name"> What's Your Location</h1>
+      <p data-ns-test="project-description">
+        {" "}
+        A login page is there and in this different location's of friends is
+        there and through emailId we can sign up this and get into this group
+      </p>
+      <h1 data-ns-test="project-name"> Thanks Doctor</h1>
+      <p data-ns-test="project-description">
+        {" "}
+        In this different names of doctor,Hospital and chemist is present and we
+        can get connected to them{" "}
+      </p>
+    </>
+  );
+};
 
 export default App;
