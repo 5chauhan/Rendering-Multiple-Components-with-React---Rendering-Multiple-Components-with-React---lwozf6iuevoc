e7779a1435246edcdd30715060b7f54ffab7bc8e
Sourabh Chauhan <97791303+5chauhan@users.noreply.github.com>
2022-04-26 14:19:57 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,23 +1,18 @@
 import React, { Component, useState } from "react";
 import "../styles/App.css";
 
-const App = () => {
-  return (
-    <>
-      <h1 data-ns-test="project-name"> What's Your Location</h1>
-      <p data-ns-test="project-description">
-        {" "}
-        A login page is there and in this different location's of friends is
-        there and through emailId we can sign up this and get into this group
-      </p>
-      <h1 data-ns-test="project-name"> Thanks Doctor</h1>
-      <p data-ns-test="project-description">
-        {" "}
-        In this different names of doctor,Hospital and chemist is present and we
-        can get connected to them{" "}
-      </p>
-    </>
-  );
-};
+class App extends Component {
+  render() {
+    return (
+      <>
+        <h1 data-ns-test="project-name">What's Your Loction</h1>
+        <p data-ns-test="project-description">
+          In this we have a group in which all friend's location is there
+        </p>
+      </>
+    );
+  }
+}
 
 export default App;
+
