bc9130985ff0026051f6fb8f7fdaae3759d5166a
Sourabh Chauhan <97791303+5chauhan@users.noreply.github.com>
2022-04-26 17:34:17 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,14 @@
-import React, { Component, useState } from "react";
-import "../styles/App.css";
+import React, {Component, useState} from "react";
+import '../styles/App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <>
-        <h1 data-ns-test="project-name">What's Your Loction</h1>
-        <p data-ns-test="project-description">
-          In this we have a group in which all friend's location is there
-        </p>
-      </>
-    );
-  }
+const App = () => {
+  return (
+    <div id="main">
+    <h1 data-ns-test="project-name">Dream Project</h1>
+    <p data-ns-test="project-description">Project Description</p>
+    </div>
+  );
 }
 
-export default App;
 
+export default App;
